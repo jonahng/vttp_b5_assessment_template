e51d0bf9f0fd62be7b298efdf80c30b094176ea0
jonahng <nyhj1998@gmail.com>
2024-10-30 10:14:59 +0800

    sorted the daily totals according to descending order

--- task01/src/vttp/batch5/sdf/task01/Main.java
+++ task01/src/vttp/batch5/sdf/task01/Main.java
@@ -20,13 +20,28 @@ public class Main {
 		//skipping over the first row of header
 		System.out.println(bufferedReader.readLine());
 
+		//creating Hashmap to store the totalCyclistsDaily and the respective BikeEntry
+		HashMap<Integer, BikeEntry> MapOfBikeEntry = new HashMap<>();
+
 		//iterating over the whole csv file
 		while((lineRead = bufferedReader.readLine()) != null){
 			String[] splitLineRead = lineRead.split(",");
+
+			//creating instance of BikeEntry
 			BikeEntry currentBikeEntry = BikeEntry.toBikeEntry(splitLineRead);
-			System.out.println(currentBikeEntry.getMonth());
+			//System.out.println(currentBikeEntry.getMonth());
+
+			int currentDayTotalCyclists = currentBikeEntry.getCasual() + currentBikeEntry.getRegistered();
+			//System.out.println("Total Cyclists for the current day is:" + currentDayTotalCyclists);
+			MapOfBikeEntry.put(currentDayTotalCyclists, currentBikeEntry);
+			
 
 		}
+
+		//Getting the list of currentDayTotalCyclists and sorting them in descending order. The number can the be referenced from the hashmap
+		List<Integer> listOfKeys = new ArrayList<>(MapOfBikeEntry.keySet());
+		Collections.sort(listOfKeys,Collections.reverseOrder());
+		System.out.println(listOfKeys);
 
 
 
