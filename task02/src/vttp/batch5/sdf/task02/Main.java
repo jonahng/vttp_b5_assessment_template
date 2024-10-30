18ffd51c4cf2cb13134cb4e6e833641bba9826e7
jonahng <nyhj1998@gmail.com>
2024-10-30 11:23:27 +0800

    reading the tic tac toe files, and can take in argument

--- task02/src/vttp/batch5/sdf/task02/Main.java
+++ task02/src/vttp/batch5/sdf/task02/Main.java
@@ -1,9 +1,38 @@
 package vttp.batch5.sdf.task02;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		//CODE TO MAKE IT TAKE ARGUMENT:
+		File boardFile;
+		if(args.length > 0){
+			boardFile = new File("task02\\TTT\\" + args[0]);
+		}else{
+			System.out.println("Restart the program and specify the file name! e.g board1.txt");
+			System.out.println("Since nothing was specified, for demo, using board0.txt");
+			boardFile = new File("task02\\TTT\\board0.txt");
+		}
 
-		System.out.printf("hello, world\n");
+		//File boardFile = new File("task02\\TTT\\board0.txt");
+		FileReader fileReader = new FileReader(boardFile);
+		BufferedReader bufferedReader = new BufferedReader(fileReader);
+		String lineRead;
+/* 		while ((lineRead = bufferedReader.readLine()) != null) {
+			System.out.println(bufferedReader.readLine());
+		} */
+
+		System.out.println(bufferedReader.readLine());
+		System.out.println(bufferedReader.readLine());
+		System.out.println(bufferedReader.readLine());
+		
+
+		//REPLACE THIS WITH THE CODE THAT TAKES IN ARGUMENT!
+
+
+
+
 	}
 }
